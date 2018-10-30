c575fc36e45cc82f7cb701e93e7208ed47f0ed36
Aditee Verma <aditee.verma50@gmail.com>
2018-10-30 04:04:39 -0400

    Registration Works Now!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { GoalsPage } from '../pages/goals/goals';
 import { WriteNowPage } from '../pages/write-now/write-now';
 import { MembershipPage } from '../pages/membership/membership';
-import {SalonsPage} from '../pages/salons/salons';
-import {DonatePage} from '../pages/donate/donate';
-import {ResourcesPage} from '../pages/resources/resources';
-import {FacebookPage} from '../pages/facebook/facebook';
-import {TwitterPage} from "../pages/twitter/twitter";
-import {LoginPage} from '../pages/login/login' ;
+import { SalonsPage } from '../pages/salons/salons';
+import { DonatePage } from '../pages/donate/donate';
+import { ResourcesPage } from '../pages/resources/resources';
+import { FacebookPage } from '../pages/facebook/facebook';
+import { TwitterPage } from "../pages/twitter/twitter";
+import { LoginPage } from '../pages/login/login' ;
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import {RegisterPage} from '../pages/register/register' ;
-import{InstagramPage} from "../pages/instagram/instagram";
-import {BlankdocPage} from "../pages/blankdoc/blankdoc";
-import {TimerPage} from "../pages/timer/timer";
-import {ProgressBarComponent} from "../components/progress-bar/progress-bar";
-import {IonicStorageModule} from "@ionic/storage";
+import { InstagramPage } from "../pages/instagram/instagram";
+import { BlankdocPage } from "../pages/blankdoc/blankdoc";
+import { TimerPage } from "../pages/timer/timer";
+import { ProgressBarComponent } from "../components/progress-bar/progress-bar";
+import { IonicStorageModule } from "@ionic/storage";
 
 import { AngularFireAuthModule} from "angularfire2/auth"
 import { AngularFireModule } from 'angularfire2';
@@ -44,7 +42,6 @@ import { BrowserTab } from "@ionic-native/browser-tab";
     InstagramPage,
     TwitterPage,
     BlankdocPage,
-    //CountdownTimerPage,
     TimerPage,
     ProgressBarComponent,
     ViewNotePage
@@ -72,7 +69,6 @@ import { BrowserTab } from "@ionic-native/browser-tab";
     BlankdocPage,
     InstagramPage,
     TwitterPage,
-    //CountdownTimerPage,
     TimerPage,
     ViewNotePage
   ],
